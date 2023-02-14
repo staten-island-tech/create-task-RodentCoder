d61283855b2d61fc84e97b581fae9277c9938de7
RodentCoder <ichen080405@gmail.com>
2023-02-14 13:10:49 -0500

    added scoring

--- CreateTask/js/dom.js
+++ CreateTask/js/dom.js
@@ -1,5 +1,6 @@
 const DOMs = {
   Rock: document.getElementById("Rock"),
+  Reset: document.getElementById("Reset"),
 };
 
 export { DOMs };
--- CreateTask/js/main.js
+++ CreateTask/js/main.js
@@ -2,11 +2,16 @@ import { DOMs } from "./dom";
 
 const hello = ["rock", "papper", "scissor"];
 
+let wins = 0;
+let ties = 0;
+let lost = 0;
+
 function rock() {
   DOMs.Rock.addEventListener("click", () => {
     let random = Math.floor(Math.random() * hello.length);
     if (hello[random] == "scissor") {
-      console.log(hello[random] + " wom");
+      wins++;
+      console.log(hello[random] + " wom" + wins);
     } else if (hello[random] == "paper") {
       console.log(hello[random] + " L");
     } else if (hello[random] == "rock") {
