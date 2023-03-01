4997cf3bb1632a5d13f97a8384165af45a98bcd7
RodentCoder <ichen080405@gmail.com>
2023-02-28 20:42:28 -0500

    Adding a functioning history system, now just needed it to not replace itself whenever the button is hit

--- CreateTask/js/main.js
+++ CreateTask/js/main.js
@@ -9,30 +9,6 @@ let lost = 0;
 
 console.log(DOMs.Result);
 
-function rock() {
-  DOMs.Rock.addEventListener("click", () => {
-    DOMs.Result.innerHTML = "";
-    let random = Math.floor(Math.random() * hello.length);
-    if (hello[random] == "scissor") {
-      DOMs.Result.insertAdjacentHTML("afterbegin", "<h2>win</h2>");
-      wins++;
-      history.push("wins:" + wins);
-      console.log(history);
-    } else if (hello[random] == "paper") {
-      DOMs.Result.insertAdjacentHTML("afterbegin", "<h2>big L</h2>");
-      lost++;
-      history.push("lost:" + lost);
-      console.log(history);
-    } else if (hello[random] == "rock") {
-      DOMs.Result.insertAdjacentHTML("afterbegin", "<h2>tie</h2>");
-      ties++;
-      history.push("ties:" + ties);
-      console.log(history);
-    }
-  });
-  DOMs.Area.insertAdjacentHTML("afterbegin", `${wins}`);
-}
-
 function flavortext(x) {
   if (x > 0) {
     console.log("works");
@@ -40,5 +16,30 @@ function flavortext(x) {
     console.log("error");
   }
 }
-flavortext(history.wins);
+
+function rock() {
+  DOMs.Rock.addEventListener("click", () => {
+    DOMs.Result.innerHTML = "";
+    let random = Math.floor(Math.random() * hello.length);
+    if (hello[random] == "scissor") {
+      DOMs.Result.insertAdjacentHTML("afterbegin", "<h2>win</h2>");
+      DOMs.Area.innerHTML = "Wins" + wins++;
+      flavortext(wins)
+     console.log(wins)
+    } else if (hello[random] == "paper") {
+      DOMs.Result.insertAdjacentHTML("afterbegin", "<h2>big L</h2>");
+      DOMs.Area.innerHTML = "Lost" + lost++;
+      flavortext(lost)
+      console.log(lost);
+    } else if (hello[random] == "rock") {
+      DOMs.Result.insertAdjacentHTML("afterbegin", "<h2>tie</h2>");
+      DOMs.Area.innerHTML = "Ties" + ties++;
+      flavortext(ties)
+      console.log(lost);
+    }
+  });
+  DOMs.Area.insertAdjacentHTML("afterbegin", `${wins}`);
+}
+
+
 rock();
