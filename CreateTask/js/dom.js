fcfb75af414ddd5a5284477cb71ad28bed0f3f7b
RodentCoder <ichen080405@gmail.com>
2023-03-04 23:50:57 -0500

    Added more buttons and a reset score thing

--- CreateTask/js/dom.js
+++ CreateTask/js/dom.js
@@ -1,5 +1,7 @@
 const DOMs = {
   Rock: document.getElementById("Rock"),
+  Paper: document.getElementById("Paper"),
+  Scissor: document.getElementById("Scissor"),
   Reset: document.getElementById("Reset"),
   Result: document.getElementById("Result"),
   Area: document.getElementById("image-area"),
--- CreateTask/js/main.js
+++ CreateTask/js/main.js
@@ -1,7 +1,6 @@
 import { DOMs } from "./dom";
 
 const hello = ["rock", "paper", "scissor"];
-let history = [];
 
 let wins = 0;
 let ties = 0;
@@ -9,7 +8,7 @@ let lost = 0;
 
 console.log(DOMs.Result);
 
-function flavortext(x) {
+function flavorwin(x) {
   if (x > 0) {
     console.log("works");
   } else {
@@ -17,29 +16,84 @@ function flavortext(x) {
   }
 }
 
-function rock() {
+
+function yes() {
+  DOMs.Area.innerHTML = "Wins" + wins + "Ties" + ties + "Lost" + lost;
   DOMs.Rock.addEventListener("click", () => {
     DOMs.Result.innerHTML = "";
     let random = Math.floor(Math.random() * hello.length);
     if (hello[random] == "scissor") {
-      DOMs.Result.insertAdjacentHTML("afterbegin", "<h2>win</h2>");
-      DOMs.Area.innerHTML = "Wins" + wins++;
-      flavortext(wins)
-     console.log(wins)
+      DOMs.Result.insertAdjacentHTML(
+        "afterbegin",
+        "<h2>Win Computer Chose Scissor</h2>"
+      );
+      DOMs.Area.innerHTML = "Wins" + wins++ + "Ties" + ties + "Lost" + lost;
     } else if (hello[random] == "paper") {
-      DOMs.Result.insertAdjacentHTML("afterbegin", "<h2>big L</h2>");
-      DOMs.Area.innerHTML = "Lost" + lost++;
-      flavortext(lost)
-      console.log(lost);
+      DOMs.Result.insertAdjacentHTML(
+        "afterbegin",
+        "<h2>Lost Computer Chose Paper</h2>"
+      );
+      DOMs.Area.insertAdjacentHTML("afterbegin", "<h2>Wins</h2>" + wins++);
+      DOMs.Area.innerHTML = "Wins" + wins + "Ties" + ties + "Lost" + lost++;
     } else if (hello[random] == "rock") {
-      DOMs.Result.insertAdjacentHTML("afterbegin", "<h2>tie</h2>");
-      DOMs.Area.innerHTML = "Ties" + ties++;
-      flavortext(ties)
-      console.log(lost);
+      DOMs.Result.insertAdjacentHTML(
+        "afterbegin",
+        "<h2>Tied Computer Chose Rock</h2>"
+      );
+      DOMs.Area.innerHTML = "Wins" + wins + "Ties" + ties++ + "Lost" + lost;
     }
   });
-  DOMs.Area.insertAdjacentHTML("afterbegin", `${wins}`);
+  DOMs.Scissor.addEventListener("click", () => {
+    DOMs.Result.innerHTML = "";
+    let random = Math.floor(Math.random() * hello.length);
+    if (hello[random] == "scissor") {
+      DOMs.Result.insertAdjacentHTML(
+        "afterbegin",
+        "<h2>Tied Computer Chose Scissor</h2>"
+      );
+      DOMs.Area.innerHTML = "Wins" + wins + "Ties" + ties++ + "Lost" + lost;
+    } else if (hello[random] == "paper") {
+      DOMs.Result.insertAdjacentHTML(
+        "afterbegin",
+        "<h2>Win Computer Chose Paper</h2>"
+      );
+      DOMs.Area.innerHTML = "Wins" + wins + "Ties" + ties + "Lost" + lost++;
+    } else if (hello[random] == "rock") {
+      DOMs.Result.insertAdjacentHTML(
+        "afterbegin",
+        "<h2>Losted Computer Chose Rock</h2>"
+      );
+      DOMs.Area.innerHTML = "Wins" + wins + "Ties" + ties + "Lost" + lost++;
+    }
+  });
+  DOMs.Paper.addEventListener("click", () => {
+    DOMs.Result.innerHTML = "";
+    let random = Math.floor(Math.random() * hello.length);
+    if (hello[random] == "scissor") {
+      DOMs.Result.insertAdjacentHTML(
+        "afterbegin",
+        "<h2>Lost Computer Chose Scissor</h2>"
+      );
+      DOMs.Area.innerHTML = "Wins" + wins + "Ties" + ties + "Lost" + lost++;
+    } else if (hello[random] == "paper") {
+      DOMs.Result.insertAdjacentHTML(
+        "afterbegin",
+        "<h2>Tied Computer Chose Paper</h2>"
+      );
+      DOMs.Area.innerHTML = "Wins" + wins + "Ties" + ties++ + "Lost" + lost;
+    } else if (hello[random] == "rock") {
+      DOMs.Result.insertAdjacentHTML(
+        "afterbegin",
+        "<h2>Win Computer Chose Rock</h2>"
+      );
+      DOMs.Area.innerHTML = "Wins" + wins++ + "Ties" + ties + "Lost" + lost;
+    }
+  });
+  DOMs.Reset.addEventListener("click", () => {
+    (wins = 0), (ties = 0), (lost = 0);
+    DOMs.Result.innerHTML = `<h2>Please Press a Button</h2>`;
+    DOMs.Area.innerHTML = "Wins" + wins + "Ties" + ties + "Lost" + lost;
+  });
 }
 
-
-rock();
+yes();
